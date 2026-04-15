0e0521e116c97a1ee28395f78ef1975612e34901
RIOKOWI <achyarrio2004@gmail.com>
2026-04-16 06:12:16 +0700

    chore: create variable and constructor

--- lib/core/shared/widgets/custom_button.dart
+++ lib/core/shared/widgets/custom_button.dart
@@ -3,7 +3,20 @@ import 'package:flutter/material.dart';
 enum ButtonVariant { primary, outlined, text }
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key});
+  final String label;
+  final VoidCallback? onPressed;
+  final bool isLoading;
+  final ButtonVariant variant;
+  final Widget? icon;
+
+  const CustomButton({
+    super.key,
+    required this.label,
+    this.onPressed,
+    this.isLoading = false,
+    this.variant = ButtonVariant.primary,
+    this.icon,
+  });
 
   @override
   Widget build(BuildContext context) {
