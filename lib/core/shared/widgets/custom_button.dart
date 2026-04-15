afe0fbbc6af9dc4b5d86c8c12da0935473eb0f99
RIOKOWI <achyarrio2004@gmail.com>
2026-04-16 06:11:29 +0700

    chore: create file and class CustomButton

---
+++ lib/core/shared/widgets/custom_button.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+enum ButtonVariant { primary, outlined, text }
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
