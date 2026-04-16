03c0e396dd2d91ab0e9d21df8e690a6b17769c03
RIOKOWI <achyarrio2004@gmail.com>
2026-04-16 07:21:26 +0700

    chore: create variable and constuctor for auth header

--- lib/core/shared/widgets/auth_header.dart
+++ lib/core/shared/widgets/auth_header.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
 
 class AuthHeader extends StatelessWidget {
-  const AuthHeader({super.key});
+  final IconData icon;
+  final String title;
+  final String subtitle;
+  final Color? iconColor;
+
+  const AuthHeader({
+    super.key,
+    required this.icon,
+    required this.title,
+    required this.subtitle,
+    this.iconColor,
+  });
 
   @override
   Widget build(BuildContext context) {
