cb75f7cec3d959ba759c11ca6dcf4f2d10348503
RIOKOWI <achyarrio2004@gmail.com>
2026-04-17 14:44:24 +0700

    feat(product): implement get products with pagination

--- lib/features/dashboard/domain/repositories/product_repository_impl.dart
+++ lib/features/dashboard/domain/repositories/product_repository_impl.dart
@@ -1,5 +1,29 @@
+import 'package:uts_1123150059/core/constants/app_constants.dart';
+import 'package:uts_1123150059/core/services/dio_client.dart';
+import 'package:uts_1123150059/features/dashboard/data/models/product_model.dart';
 import 'package:uts_1123150059/features/dashboard/domain/repositories/product_repository.dart';
 
 class ProductRepositoryImpl extends ProductRepository {
+  @override
+  Future<List<ProductModel>> getProducts({
+    int page = 1,
+    int limit = 10,
+    String? category,
+  }) async {
+    final response = await DioClient.instance.get(
+      AppConstants.products,
+      queryParameters: {'page': page, 'limit': limit, 'category': category},
+    );
 
+    final List<dynamic> data = response.data['data'];
+    return data.map((e) => ProductModel.fromJson(e)).toList();
+  }
+
+  @override
+  Future<ProductModel> getProductById(int id) async {
+    final response = await DioClient.instance.get(
+      '${AppConstants.products}/$id',
+    );
+    return ProductModel.fromJson(response.data['data']);
+  }
 }
