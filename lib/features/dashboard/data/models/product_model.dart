496db15b378cac4f12ef2a82b75b58fe6346ca16
RIOKOWI <achyarrio2004@gmail.com>
2026-04-17 14:18:04 +0700

    feat(product): create variable and constructor for product model

--- lib/features/dashboard/data/models/product_model.dart
+++ lib/features/dashboard/data/models/product_model.dart
@@ -1,5 +1,20 @@
 import 'package:equatable/equatable.dart';
 
 class ProductModel extends Equatable {
+  final int    id;
+  final String name;
+  final double price;
+  final String imageUrl;
+  final String category;
+
+  const ProductModel({
+    required this.id,
+    required this.name,
+    required this.price,
+    required this.imageUrl,
+    required this.category,
+  });
+
+  
 
 }
