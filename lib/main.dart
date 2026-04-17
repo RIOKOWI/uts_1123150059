d9d2b8d545e4691d48026919c5d44c2661e22ea5
RIOKOWI <achyarrio2004@gmail.com>
2026-04-17 13:55:36 +0700

    style(main): implement theme, routes, and title app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:uts_1123150059/core/constants/app_constants.dart';
+import 'package:uts_1123150059/core/constants/app_strings.dart';
+import 'package:uts_1123150059/core/routes/app_router.dart';
+import 'package:uts_1123150059/core/theme/app_theme.dart';
 import 'package:uts_1123150059/features/auth/presentation/providers/auth_provider.dart';
 import 'firebase_options.dart';
 
@@ -33,7 +37,15 @@ class MyApp extends StatelessWidget {
       providers: [
         ChangeNotifierProvider(create: (_) => AuthProvider()),
         ChangeNotifierProvider(create: (_) => ProductProvider()),  
-      ]
+      ],
+      child: MaterialApp(
+        title:                  AppStrings.appName,
+        debugShowCheckedModeBanner: false,
+        theme:                  AppTheme.light,
+        initialRoute:           AppRouter.splash,
+        routes:                 AppRouter.routes,
+      ),
     );
+
   }
 }
