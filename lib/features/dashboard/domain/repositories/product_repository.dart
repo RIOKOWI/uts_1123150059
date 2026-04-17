1aa0e9617b508ab7759aab2912a14a81d65b11d0
RIOKOWI <achyarrio2004@gmail.com>
2026-04-17 14:40:20 +0700

    feat(product): get products with pagination

--- lib/features/dashboard/domain/repositories/product_repository.dart
+++ lib/features/dashboard/domain/repositories/product_repository.dart
@@ -1,3 +1,7 @@
+import 'package:uts_1123150059/features/dashboard/data/models/product_model.dart';
+
 abstract class ProductRepository {
-  
+  Future<List<ProductModel>> getProducts({int page = 1, int limit = 10, String? category});
+  Future<ProductModel> getProductById(int id);
+
 }
