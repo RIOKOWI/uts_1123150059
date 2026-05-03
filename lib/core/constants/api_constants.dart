363f1dfb43360201b3aace041b5eeb55671a7307
RIOKOWI <achyarrio2004@gmail.com>
2026-05-03 13:04:04 +0700

    feat: add endpoint cart

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,15 +1,17 @@
-class ApiConstants{
+class ApiConstants {
   // http://192.168.100.5:8080/v1
   static const String baseUrl = 'http://localhost:8080/v1';
- 
+
   // Auth endpoints
   static const String verifyToken = '/auth/verify-token';
- 
+
   // Product endpoints
   static const String products = '/products';
- 
+
+  // Cart endpoints
+  static const String cart = '/cart';
+
   // Timeout
   static const int connectTimeout = 15000;
   static const int receiveTimeout = 15000;
-
 }
